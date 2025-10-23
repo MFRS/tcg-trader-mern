ad0ddb96a17bed5500e7be1eb1ab9b1265a1643c
Miguel Salvador <miguel_f_r_salvador@hotmail.com>
2025-10-23 16:12:14 +0100

    Add dotenv configuration to server and database connection files

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import records from "./routes/records.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+
+
+
 
 app.use(cors());
 app.use(express.json());;
